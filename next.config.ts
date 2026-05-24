917a57531bed209aeedad6a1ce5cc0d3a8df657b
paceyroth-rgb <paceyroth@gmail.com>
2026-05-24 12:59:12 -0300

    Album artowrk bug fix

--- next.config.ts
+++ next.config.ts
@@ -4,4 +4,21 @@ const nextConfig: NextConfig = {
   /* config options here */
 };
 
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "yt3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
+
 export default nextConfig;
